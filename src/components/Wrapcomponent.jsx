abb4c64e6ae8a44a4faf2c6450909655510c8e50
KinHongB <ghdql0418@naver.com>
2023-05-19 17:08:12 +0900

    FILA

---
+++ src/components/Wrapcomponent.jsx
@@ -0,0 +1,12 @@
+import Section3Component from "./wrap/Section3Component";
+import Section5Component from "./wrap/Section5Component";
+import React from 'react';
+
+export default function Wrapcomponent(){
+    return (
+        <div>
+            <Section3Component />
+            <Section5Component />
+        </div>
+    );
+};
